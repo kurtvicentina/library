7186d8b71fcb1073a578e284773ce6de7026b976
kurtvicentina <kerteee32@gmail.com>
2024-05-28 17:06:15 +0800

    figuring out how to connect the object into every data into the DOM

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let showBookButton = document.querySelector('#showBookButton');
 let addBookModal = document.querySelector('#addBookModal');
 
 let bookForm = document.querySelector('#bookForm');
+let booksContainer = document.querySelector('.books-container');
 let titleInput = document.querySelector('#title');
 let authorInput = document.querySelector('#author');
 let pagesInput = document.querySelector('#pages');
-let readStatus = document.querySelector('#status');
+let readStatus = document.querySelector('#switch');
+let notRead = document.querySelector('#notRead');
+
+
+let book;
 
 let cancelButton = document.querySelector('#cancelBookButton');
 let submitButton = document.querySelector('#addBookButton');
@@ -25,10 +30,15 @@ function Book(title, author, pages, status) {
 
 
 submitButton.addEventListener('click', (e) => {
-    let userInput = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus.value)
+    if(readStatus.checked){
+        book = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus.value);
+    }
+    else{
+        book = new Book(titleInput.value, authorInput.value, pagesInput.value, notRead.value);
+    }
 
-    myLibrary.push(userInput);
     e.preventDefault();
+    myLibrary.push(book);
     addBookModal.close()
     bookForm.reset();
     addBookToLibrary();
@@ -36,11 +46,57 @@ submitButton.addEventListener('click', (e) => {
 
 cancelButton.addEventListener('click', () =>{
     addBookModal.close();
-});  
+});
+
+// readStatus.addEventListener('change', changeReadStatus())
+
+// function changeReadStatus(e) {
+//     if(readStatus.checked){
+//         e.status = readStatus.value
+//     }
+//         e.status = notRead.value
+// }
 
 function addBookToLibrary(){
     
-    console.log(myLibrary
-        
-    )
-}
+    console.log(myLibrary);
+
+    let newBookContainer = document.createElement('div');
+    let newBookHeader = document.createElement('h3');
+    let newBookAuthor = document.createElement('p');
+    let newBookPage = document.createElement('p');
+    let newBookReadStatus = document.createElement('p');
+    let newLabelContainer = document.createElement('label');
+    let newLabel = document.createElement('label');
+    let newSwitch = document.createElement('input');
+    newSwitch.type = 'checkbox';
+    let newSlider = document.createElement('span');
+
+    newBookHeader.textContent = book.title;
+    newBookAuthor.textContent = book.author;
+    newBookPage.textContent = book.pages;
+    newBookReadStatus.textContent = book.status;
+
+    newBookContainer.setAttribute('class', 'book');
+    newBookHeader.setAttribute('class', 'title-container');
+    newBookPage.setAttribute('class', 'author');
+    newBookAuthor.setAttribute('class', 'pages');
+    newBookReadStatus.setAttribute('class', 'status');
+    newLabelContainer.setAttribute('class', 'status-label');
+    newLabel.setAttribute('class', 'switch');
+    newSwitch.setAttribute('id', 'switch');
+    newSlider.setAttribute('class', 'slider');
+
+
+    newBookReadStatus.append(newLabelContainer);
+    newBookReadStatus.append(newLabel);
+    newLabel.append(newSwitch);
+    newLabel.append(newSlider);
+
+    newBookContainer.append(newBookHeader);
+    newBookContainer.append(newBookAuthor);
+    newBookContainer.append(newBookPage);
+    newBookContainer.append(newBookReadStatus);
+
+    booksContainer.append(newBookContainer)
+};
